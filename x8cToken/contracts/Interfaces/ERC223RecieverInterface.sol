c6e4eb65c1b4f72fa122a1ecd8702271862996af
x8Dev <ervin@ionectar.com>
2018-01-09 20:49:31 +0100

    Delete

--- x8cToken/contracts/Interfaces/ERC223RecieverInterface.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.4.13;
-
-contract ERC223RecieverInterface {
-    function tokenFallback(address _from, uint _value, bytes _data) public;
-}
--- x8cToken/contracts/Interfaces/TokenRecipientInterface.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.4.13;
-
-contract tokenRecipientInterface {
-  function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData);
-}
